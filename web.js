ca1b02fac7e0b6fef044b17508bc0a26f4a9736b
MadaManu <madamanu_irl@yahoo.com>
2014-07-28 19:29:26 +0100

    testing mongo

--- web.js
+++ web.js
@@ -2,6 +2,10 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var mongo = require('mongodb');
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
 
 app.use(logfmt.requestLogger());
 
@@ -9,23 +13,24 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/add/:name', function(req, res) {
+  mongo.Db.connect(mongoUri, function (err, db) {
+    db.collection('first', function(er, collection) {
+      collection.insert({'name':req.param("name"), function (er,rs){
+      });
+    });
+  });
+  res.send(req.param("name") + ' was added to database!');
+});
+
+app.get('/alldata', function(req, res) {
+  mongo.Db.connect(mongoUri, function (err, db) {
+    res.send(db.first.find());
+  });
+});
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-
-
-var mongo = require('mongodb');
-
-var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mydb';
-
-mongo.Db.connect(mongoUri, function (err, db) {
-  db.collection('mydocs', function(er, collection) {
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-    });
-  });
-});
