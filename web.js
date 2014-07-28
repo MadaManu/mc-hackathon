bd7567d03839ffb091880a6926e77814cbc7b65d
MadaManu <madamanu_irl@yahoo.com>
2014-07-28 19:15:57 +0100

    trying out mongo

--- web.js
+++ web.js
@@ -9,11 +9,23 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.get('/:name', function(req, res) {
-  res.send('Hello, ' + req.param("name") + '!');
-});
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+
+
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
 });
