5d81e072b8b4988c00b7635a1851b6b114f07c1d
MadaManu <madamanu_irl@yahoo.com>
2014-07-30 19:11:55 +0100

    Start developing

--- web.js
+++ web.js
@@ -1,38 +1,44 @@
 
-var express = require("express");
+// var express = require("express");
+var fortune = require("fortune");
+// app=fortune({adapter: 'mongodb'});
 var logfmt = require("logfmt");
-var app = express();
-var mongo = require('mongodb');
-var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mydb';
 
-app.use(logfmt.requestLogger());
+var datastore = require("mongodb");
+var db = new datastore();
 
-app.get('/', function(req, res) {
-  res.send('This is the magic laptop speaking!!!');
-});
+// var app = express();
+// var mongo = require('mongodb');
+// var mongoUri = process.env.MONGOLAB_URI ||
+//   process.env.MONGOHQ_URL ||
+//   'mongodb://localhost/mydb';
 
-app.get('/add/:name', function(req, res) {
-  mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('first', function(er, collection) {
-      collection.insert({'name':req.param("name")}, {safe: true}, function (er,rs){
-      });
-    });
-  });
-  res.send(req.param("name") + ' was added to database!');
-});
+// app.use(logfmt.requestLogger());
 
-app.get('/alldata', function(req, res) {
-  var data = "Maybe nothing fetched";
-  mongo.Db.connect(mongoUri, function (err, db) {
-    data = db.getCollectionNames();
-  });
-  res.send(data);
-});
+// app.get('/', function(req, res) {
+//   res.send('Fuck off!!!');
+// });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+// app.get('/add/:name', function(req, res) {
+//  mongo.Db.connect(mongoUri, function (err, db) {
+//    db.collection('first', function(er, collection) {
+//      collection.insert({'name':req.param("name")}, {safe: true}, function (er,rs){
+//      });
+//    });
+//  });
+//  res.send(req.param("name") + ' was added to database!');
+//});
+
+// app.get('/alldata', function(req, res) {
+//  var data = "Maybe nothing fetched";
+//  mongo.Db.connect(mongoUri, function (err, db) {
+//    data = db.getCollectionNames();
+//  });
+//  res.send(data);
+// });
+
+// var port = Number(process.env.PORT || 5000);
+// app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
 
