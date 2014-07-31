2796c6d61deaf18c9c0762b6784657e87ad1c0ce
MadaManu <madamanu_irl@yahoo.com>
2014-07-31 12:17:53 +0100

    mongoDB first structure including a basic user model

---
+++ config.js
@@ -0,0 +1,7 @@
+// constructor
+function config() {
+}
+
+module.exports = config;
+
+config.mongoUri = 'mongodb://car_pass:Password@ds053419.mongolab.com:53419/heroku_app27894287';
--- web.js
+++ web.js
@@ -1,21 +1,74 @@
 var express = require("express");
 var logfmt = require("logfmt");
-
-
-// var fortune = require("fortune");
-// app=fortune({adapter: 'mongodb'});
-var datastore = require("mongodb");
-//var db = new datastore();
-
+var mongoose   = require('mongoose');
+var config = require("./config");
+var User = require('./models/user');
+var bodyParser = require('body-parser');
 var app = express();
-var mongo = require('mongodb');
-var mongoUri = mongodb://<dbuser>:<dbpassword>@ds053419.mongolab.com:53419/heroku_app27894287
+mongoose.connect(config.mongoUri);
 
+// mongo.connect(config.mongoUri, function(err, db) {
+// 	if (err) {
+// 		return console.dir(err);
+// 	}
+// 	db.createCollection('test', function(err, collection) {
+// 		if (err) {
+// 			return console.dir(err);
+// 		}
+// 		// db.collection('test', function(err, collection) {}
+// 		return console.dir("YEEEEEEY!!!!");
+// 	});
+// });
+
+var User = require('./models/user');
+
+app.use(bodyParser());
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('mada :) !!!');
+  res.json("Wellcome to the api!");
 });
+
+
+app.post('/users', function(req, res) {
+	var user = new User(); 		// create a new instance of the User model
+		// console.dir(req.body.name);
+		user.name = req.body.name;  // set the bears name (comes from the request)
+		user.password = req.body.password;
+
+		user.save(function(err) {
+			if (err) {
+				res.send(err);
+			}
+			res.json({ message: 'User created!' });
+		});
+});
+
+app.get('/users', function(req, res) {
+	User.find(function(err, users) {
+		if (err)
+			res.send(err);
+		res.json(users);
+	});
+});
+
+
+app.get('/users/:id', function(req, res) {
+	User.findById(req.params.id, function(err, User) {
+		if (err)
+			res.send(err);
+		res.json(User);
+	});
+});
+
+
+// app.configure(function () {
+//   app.use(express.bodyParser());
+//   app.use(express.methodOverride());
+//   app.use(app.router);
+//   app.use(express.static(path.join(application_root, "public")));
+//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+// });
 
 // app.get('/add/:name', function(req, res) {
 //  mongo.Db.connect(mongoUri, function (err, db) {
